8cc0291870e009ad9c2b9bf93f5611a23b0ba54d
Cap0011 <5165511@naver.com>
2021-09-17 02:05:24 +0900

    Added signin/signup screen

--- app/src/main/java/com/example/managect/IntroActivity.kt
+++ app/src/main/java/com/example/managect/IntroActivity.kt
@@ -1,8 +1,10 @@
 package com.example.managect
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_intro.*
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -11,7 +13,20 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         val typeFace: Typeface = Typeface.createFromAsset(assets, "NeutronsDemoRegular.ttf")
         tv_intro_app_name.typeface = typeFace
+
+        btn_intro_signup.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        btn_intro_signin.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/managect/SignInActivity.kt
@@ -0,0 +1,32 @@
+package com.example.managect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setUpActionBar()
+    }
+
+    private fun setUpActionBar(){
+        setSupportActionBar(tb_signin)
+
+        val actionBar = supportActionBar
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+        }
+
+        tb_signin.setNavigationOnClickListener{ onBackPressed()}
+    }
+}
